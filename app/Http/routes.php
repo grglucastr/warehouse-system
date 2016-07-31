e0d6ec00d2a37e1a2dd5245c68ac2c04f877001a
George Bentes <george.bentes@gmail.com>
2016-07-31 14:55:03 -0400

    adicionei novas rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,13 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+
+Route::get('/', 'TransferenciaWarehouseController@index');
+Route::get('/realizar-transferencia',
+           'TransferenciaWarehouseController@goPageRealizarTransferencia');
